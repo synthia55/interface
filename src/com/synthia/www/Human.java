89d2e8286331462f43e85650c27a89b42865f4d4
synthia <synthiacseiiuc@gamil.com>
2018-07-22 02:16:03 -0700

    initial commit

---
+++ src/com/synthia/www/Human.java
@@ -0,0 +1,18 @@
+package com.synthia.www;
+
+public interface class Human {
+    package com.iiuc.sdmag.www;
+
+     int head=2;
+     int hand=2;
+     int legs=2;
+
+
+     public void sleeps();
+     public void eats();
+
+
+
+
+    }
+}
---
+++ src/com/synthia/www/Main.java
@@ -0,0 +1,4 @@
+package com.synthia.www;
+
+public class Main {
+}
